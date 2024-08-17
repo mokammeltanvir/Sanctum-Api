a709c72ab3f0ca7478a1caeece0ffa09c610c75f
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-08-17 22:56:09 +0600

    Post CRUD API

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -12,7 +14,13 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $data['posts'] = Post::all();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post List',
+            'data' => $data,
+        ], 200);
     }
 
     /**
@@ -20,7 +28,36 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $ValidateUser = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($ValidateUser->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $ValidateUser->errors(),
+            ], 401);
+        }
+
+        $img = $request->image;
+        $ext = $img->getClientOriginalExtension();
+        $imageName = time() . '.' . $ext;
+        $img->move(public_path() . '/uploads/', $imageName);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $imageName,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post Created Successfully',
+            'post' => $post,
+        ], 200);
+
     }
 
     /**
@@ -28,7 +65,13 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data['post'] = Post::select('id', 'title', 'description', 'image')->where('id', $id)->get();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post Details',
+            'data' => $data,
+        ], 200);
     }
 
     /**
@@ -36,14 +79,71 @@ class PostController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $ValidateUser = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($ValidateUser->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $ValidateUser->errors(),
+            ], 401);
+        }
+
+        $postImage = Post::select('id', 'image')->where('id', $id)->get();
+
+        if($request->image != ''){
+            $path = public_path() . '/uploads/';
+
+            if($postImage[0]->image != '' && $postImage[0]->image != null){
+                $old_file = $path . $postImage[0]->image;
+                if(file_exists($old_file)){
+                    unlink($old_file);
+        }
+
+        }
+        $img = $request->image;
+        $ext = $img->getClientOriginalExtension();
+        $imageName = time() . '.' . $ext;
+        $img->move(public_path() . '/uploads/', $imageName);
+
+    }else{
+        $imageName = $postImage->image;
     }
+
+        $post = Post::where('id', $id)->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $imageName,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post Updated Successfully',
+            'post' => $post,
+        ], 200);
+    }
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+
+        $imagePath = Post::select('image')->where('id', $id)->get();
+        $filePath = public_path() . '/uploads/' . $imagePath[0]['image'];
+
+        unlink($filePath);
+
+        $post = Post::where('id', $id)->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post Deleted Successfully',
+            'post' => $post,
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
 
 Route::post('signup', [AuthController::class, 'signup']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('posts', PostController::class);
+});
